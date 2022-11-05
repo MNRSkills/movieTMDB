1553bed1a5613c2d2bcf777e3c8acf91c399572b
Mack Ramirez <mnrdev510@gmail.com>
2022-11-05 07:04:33 -0700

    including all files

---
+++ README.md
@@ -0,0 +1,5 @@
+# WORKING WITH TMDB API
+
+## MOVIES BY POPULAR DEMAND
+
+-- connecting the api with axios for sec key purpose
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config();
 const app = express();
+const PORT = process.env.PORT || 7900;
 
-app.listen(4000, () => console.log("We are listening on port 4000"));
+const popularRouter = require("./routes/popular");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", popularRouter);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
