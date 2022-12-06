aa759cf753b61183d0cdc70509321c4e14a8a5cd
Mack Ramirez <mnrdev510@gmail.com>
2022-12-05 21:29:42 -0800

    found the fix to my re-render issue but needs more work

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,8 @@
 const search = require("express").Router();
 const axios = require("axios");
-const { json } = require("express");
+// const { json } = require("express");
 
 search.get("/search/:query", async (req, res) => {
-  console.log("WASSUP", req.body);
   console.log("PARAMS", req.params.query);
   try {
     await axios
@@ -11,6 +10,7 @@ search.get("/search/:query", async (req, res) => {
         `/search/movie?api_key=${process.env.TMDB_API_KEY}&language=en-US&query=${req.params.query}&page=1&include_adult=false`
       )
       .then((response) => {
+        console.log("THIS IS THE QUERY", response.data);
         res.json(response.data);
       });
   } catch (error) {
